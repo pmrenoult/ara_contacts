c2996846f0fa0eddc0bc17e4f4d7ba087264aee3
Pierre-Maxence Renoult <renoultpm@gmail.com>
2016-12-29 10:27:43 +0100

    Ajouté le scaffold relatif aux contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229090344) do
+ActiveRecord::Schema.define(version: 20161229091631) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
